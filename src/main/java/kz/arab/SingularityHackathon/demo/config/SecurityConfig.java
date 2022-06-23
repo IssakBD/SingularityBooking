d20bea2b86d3c17e80f179014722f0a367dc6341
admin <administrator@jits-3-28.jits.ipa.dc>
2022-06-23 18:17:19 +0600

    cross try 5

--- src/main/java/kz/arab/SingularityHackathon/demo/config/SecurityConfig.java
+++ src/main/java/kz/arab/SingularityHackathon/demo/config/SecurityConfig.java
@@ -7,47 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import javax.servlet.http.HttpServletResponse;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
 
     private static final String ADMIN_ENDPOINT = "/api/v1/admin/**";
     private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
 
-
     @Autowired
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("");
-        config.addAllowedHeader("");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("PATCH");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return source;
     }
 
     @Bean
@@ -59,49 +33,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
+                .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/v1/register", "/api/v1/auth/login", "/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**").permitAll()
                 .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated();
-
-        http.apply(new JwtConfigurer(jwtTokenProvider));
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
     }
-
-
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//        http.cors()
-//                .and()
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/auth/*").permitAll()
-//                .antMatchers("/limits/").permitAll()
-//                .antMatchers("/instance/").permitAll()
-//                .antMatchers("/consul/").permitAll()
-//                .antMatchers("/swagger-ui/").permitAll()
-//
-//                .antMatchers("/swagger-resources/").permitAll()
-//                .antMatchers("/v2/").permitAll()
-//                .antMatchers("/report/").permitAll()
-//                .antMatchers("/admin/poll/").permitAll()
-//                .antMatchers("/v3/").permitAll()
-//                .antMatchers("/docs/").permitAll()
-//                .antMatchers("/error").permitAll()
-//                .anyRequest().authenticated();
-//
-//        http.exceptionHandling().accessDeniedHandler(
-//                (request, response, accessDeniedException) -> response.setStatus(HttpServletResponse.SC_UNAUTHORIZED)
-//        );
-//
-//        http.apply(tokenFilterConfigurer);
-//    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
