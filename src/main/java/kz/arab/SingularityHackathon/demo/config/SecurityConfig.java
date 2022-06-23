1d0834c8ad2d1bdd42919c72b328bd3d696d9fbf
admin <administrator@jits-3-28.jits.ipa.dc>
2022-06-23 17:16:27 +0600

    add cors()

--- src/main/java/kz/arab/SingularityHackathon/demo/config/SecurityConfig.java
+++ src/main/java/kz/arab/SingularityHackathon/demo/config/SecurityConfig.java
@@ -12,12 +12,15 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
 
     private static final String ADMIN_ENDPOINT = "/api/v1/admin/**";
     private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
+
 
     @Autowired
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
@@ -32,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -40,10 +43,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/v1/register", "/api/v1/auth/login", "/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**").permitAll()
                 .antMatchers("/api/v1/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .apply(new JwtConfigurer(jwtTokenProvider));
+                .anyRequest().authenticated();
+
+        http.apply(new JwtConfigurer(jwtTokenProvider));
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.cors()
+//                .and()
+//                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/auth/*").permitAll()
+//                .antMatchers("/limits/").permitAll()
+//                .antMatchers("/instance/").permitAll()
+//                .antMatchers("/consul/").permitAll()
+//                .antMatchers("/swagger-ui/").permitAll()
+//
+//                .antMatchers("/swagger-resources/").permitAll()
+//                .antMatchers("/v2/").permitAll()
+//                .antMatchers("/report/").permitAll()
+//                .antMatchers("/admin/poll/").permitAll()
+//                .antMatchers("/v3/").permitAll()
+//                .antMatchers("/docs/").permitAll()
+//                .antMatchers("/error").permitAll()
+//                .anyRequest().authenticated();
+//
+//        http.exceptionHandling().accessDeniedHandler(
+//                (request, response, accessDeniedException) -> response.setStatus(HttpServletResponse.SC_UNAUTHORIZED)
+//        );
+//
+//        http.apply(tokenFilterConfigurer);
+//    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
--- src/main/java/kz/arab/SingularityHackathon/demo/controller/ModifyUserRestControllerV1.java
+++ src/main/java/kz/arab/SingularityHackathon/demo/controller/ModifyUserRestControllerV1.java
@@ -1,9 +1,10 @@
-//package kz.arab.SingularityHackathon.demo.controller;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/api/v1/auth")
-//public class ModifyUserRestControllerV1 {
-//}
+package kz.arab.SingularityHackathon.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class ModifyUserRestControllerV1 {
+
+}
